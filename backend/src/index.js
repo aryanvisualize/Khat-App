77af2ae6e1c84cd0770d54a0c22005059b95e229
aryanvisualize <aryanvisualize@gmail.com>
2026-08-02 19:58:58 +0530

    implement user and message models, add database connection, and update package dependencies

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,23 @@
-import express from 'express';
-import 'dotenv/config';
+import express from "express";
+import "dotenv/config";
+import User from "./models/user.model.js";
+import { connectDB } from "./lib/db.js";
+import { clerkMiddleware } from "@clerk/express";
+import cors from "cors";
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+const FRONTEND_URL = process.env.FRONTEND_URL;
 
-console.log("DB_URL=",process.env.DB_URL)
+app.use(express.json());
+app.use(clerkMiddleware());
+app.use(cors({ origin: FRONTEND_URL, credentials: true }));
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "ok" });
+});
+
 app.listen(PORT, () => {
+  connectDB();
   console.log(`Server is running on http://localhost:${PORT}`);
 });
