ece7bdc9f592fdacc7ccbf5219e374c8991f887b
aryanvisualize <aryanvisualize@gmail.com>
2026-08-03 13:25:58 +0530

    add Clerk webhook handling and integrate with user model for updates and deletions

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,9 @@ import "dotenv/config";
 import fs from "fs";
 import path from "path";
 
+import job from "./cronjob.js";
+import clerkWebhook from "./webhooks/clerk.webhook.js";
+
 import User from "./models/user.model.js";
 import { connectDB } from "./lib/db.js";
 
@@ -15,6 +18,8 @@ const PORT = process.env.PORT || 3000;
 const FRONTEND_URL = process.env.FRONTEND_URL;
 
 const publicDir = path.join(process.cwd(), "public");
+
+app.use('api/webhooks/clerk',express.raw({type:"application/json"}) ,clerkWebhook)
 
 app.use(express.json());
 app.use(clerkMiddleware());
