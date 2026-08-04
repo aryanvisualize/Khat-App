5354a95aa3d81fa4f9c4175214189f7a3b18670d
aryanvisualize <aryanvisualize@gmail.com>
2026-08-04 15:41:30 +0530

    feat: add ImageKit integration and update package dependencies

---
+++ backend/src/lib/imagekit.js
@@ -0,0 +1,13 @@
+import ImageKit, {toFile} from "@imagekit/nodejs";
+
+const imagekit = new ImageKit({privateKey: process.env.IMAGEKIT_PRIVATE_KEY});
+
+function hasImageKitConfig(){
+    return Boolean(process.env.IMAGEKIT_PRIVATE_KEY);
+}
+
+function createFileName(originalName = "upload") {
+  const safeName = originalName.replace(/[^a-zA-Z0-9._-]/g, "_");
+  return `chat-${Date.now()}-${safeName}`;
+}
+
