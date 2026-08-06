1b928c1a598e15a7bd9f55cda395cf117fee6035
aryanvisualize <aryanvisualize@gmail.com>
2026-08-06 15:22:24 +0530

    feat: update package dependencies and implement new components for authentication and theming

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,22 @@
 import {WallpaperProvider} from "./context/WallpaperContext.jsx";
 import {ThemeProvider} from "./context/ThemeContext.jsx";
-import { Navigate, Route, Routes } from 'react-router';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import ChatPage from "./pages/ChatPage.jsx"
 import AuthPage from "./pages/AuthPage.jsx";
 import { useAuth } from "@clerk/react";
+import PageLoader from "./components/PageLoader.jsx";
 
 function App() {
 
   const { isSignedIn, isLoaded } = useAuth();
 
-  if(!isLoaded) return <p>Loading...</p>
+  if(!isLoaded) return <PageLoader/>;
   return (
     <ThemeProvider>
       <WallpaperProvider>
           <Routes>
             <Route path="/" element={ isSignedIn ? <ChatPage/> : <Navigate to={"/auth"} replace  />} />
-            <Route path="/auth" element={ !isSignedIn ? <AuthPage/> : <Navigate to={"/"} replace  />} />
+            <Route path="/auth" element={!isSignedIn ? <AuthPage /> : <Navigate to={"/"} replace />}/>
           </Routes>
       </WallpaperProvider>
     </ThemeProvider>
